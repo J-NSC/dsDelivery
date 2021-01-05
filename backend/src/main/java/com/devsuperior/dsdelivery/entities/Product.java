c9903a0ac809167cad4d6911635c78c7d277397f
sevirotimo <jncosta201@gmail.com>
2021-01-05 00:05:37 -0300

    Initial setup

---
+++ backend/src/main/java/com/devsuperior/dsdelivery/entities/Product.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dsdelivery.entities;
+
+public class Product {
+	private Long id;
+	private String name;
+	private Double price;
+	private String description;
+	private String imageUri;
+}
